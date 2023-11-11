32cfb945b94e66ae06f8112f3523777be7dd87fc
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-11-11 23:23:32 +0200

    adding

---
+++ 01. Linear Data Structures Lab/02. Queue/Problem02.Stack/Stack.cs
@@ -0,0 +1,97 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    public class Stack<T> : IAbstractStack<T>
+    {
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+                : this(element, null)
+            {
+                Element = element;
+            }
+        }
+
+        private Node top;
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            var nod = new Node(item, top);
+            top = nod;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("colection is Empty");
+            }
+            var oldTop = top;
+            if (top.Next != null)
+            {
+                top = top.Next;
+            }
+            else
+            {
+                top = null;
+            }
+
+            Count--;
+            return oldTop.Element;
+        }
+
+        public T Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("colection is Empty");
+            }
+            return top.Element;
+        }
+
+        public bool Contains(T item)
+        {
+            var note = top;
+            while (note != null)
+            {
+                if (item.Equals(note.Element))
+                {
+                    return true;
+                }
+
+                note = note.Next;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var note = top;
+            while(note != null)
+            {
+                yield return note.Element;
+                note = note.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+
+
+}

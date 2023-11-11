b2306849e4fa5be9c195c1bed081c95a0f730841
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-11-11 23:48:50 +0200

    adding

---
+++ 01. Linear Data Structures Lab/03. Queue/Problem03.Queue/Queue.cs
@@ -0,0 +1,99 @@
+﻿namespace Problem03.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Queue<T> : IAbstractQueue<T>
+    {
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+                : this(element, null)
+            {
+                Element = element;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T item)
+        {
+            if (head == null)
+            {
+                head = new Node(item);
+                Count++;
+                return;
+            }
+            var node = head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = new Node(item);
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var node = head;
+            head = head.Next;
+            Count--;
+            return node.Element;
+        }
+
+        public T Peek()
+        {
+            if(head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            return head.Element;
+        }
+
+        public bool Contains(T item)
+        {
+            var node = head;
+            while (node != null)
+            {
+                if (item.Equals(node.Element))
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = head;
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}

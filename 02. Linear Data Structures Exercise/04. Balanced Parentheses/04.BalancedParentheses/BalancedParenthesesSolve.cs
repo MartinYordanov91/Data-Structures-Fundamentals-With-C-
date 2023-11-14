3d2a9b7e33fffef78a75b20a0644038e943a0c5a
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-11-14 20:11:04 +0200

    adding

---
+++ 02. Linear Data Structures Exercise/04. Balanced Parentheses/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            Stack<char> chars = new Stack<char>();
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                if (new char[3] { ')', '}', ']' }.Contains(parentheses[i]))
+                {
+                    if (parentheses[i] == ')' && chars.Any() &&chars.Peek() == '(')
+                    {
+                        chars.Pop();
+                        continue;
+                    }
+                    else if (parentheses[i] == '}' && chars.Any() && chars.Peek() == '{')
+                    {
+                        chars.Pop();
+                        continue;
+                    }
+                    else if (parentheses[i] == ']' && chars.Any() && chars.Peek() == '[')
+                    {
+                        chars.Pop();
+                        continue;
+                    }
+                    
+                }
+
+                chars.Push(parentheses[i]);
+            }
+
+            return chars.Any() ? false : true;
+        }
+    }
+}

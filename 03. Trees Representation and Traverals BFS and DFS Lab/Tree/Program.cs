fd3ce4d1d4ce62e704e516f083b0c2ccd9f25cc3
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-11-15 18:52:28 +0200

    adding

---
+++ 03. Trees Representation and Traverals BFS and DFS Lab/Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new Tree<int>(10,  new Tree<int>(20,   new Tree<int>(30),
+                                                             new Tree<int>(31),
+                                                             new Tree<int>(32)),
+
+                                         new Tree<int>(21  , new Tree<int>(33),
+                                                             new Tree<int>(34),
+                                                             new Tree<int>(35)),
+
+                                         new Tree<int>(22,   new Tree<int>(36),
+                                                             new Tree<int>(37)));
+
+            Console.WriteLine(string.Join(", ", tree.OrderBfs()));
+            tree.Swap(20,37);
+            Console.WriteLine(string.Join(", ", tree.OrderBfs()));
+            
+        }
+    }
+}
---
+++ 03. Trees Representation and Traverals BFS and DFS Lab/Tree/Tree.cs
@@ -0,0 +1,170 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> children;
+        private T value;
+        private Tree<T> parent;
+
+        public Tree(T value)
+        {
+            this.value = value;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] childrens)
+            : this(value)
+        {
+            foreach (var child in childrens)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            var parentNode = this.FindNodeWhitBfs(parentKey);
+
+            if (parentNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            parentNode.children.Add(child);
+            child.parent = parentNode;
+        }
+
+        private Tree<T> FindNodeWhitBfs(T parentKey)
+        {
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+                if (subtree.value.Equals(parentKey))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private void Dfs(Tree<T> node, ICollection<T> result)
+        {
+
+            foreach (var child in node.children)
+            {
+                this.Dfs(child, result);
+            }
+            result.Add(node.value);
+
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+            var list = new List<T>();
+            Dfs(this, list);
+            return list;
+        }
+        public IEnumerable<T> OrderBfs()
+        {
+            var queue = new Queue<Tree<T>>();
+            var result = new List<T>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+                result.Add(subtree.value);
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<T> SteckOrderDfs()
+        {
+            var stack = new Stack<Tree<T>>();
+            var result = new Stack<T>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+
+                foreach (var child in node.children)
+                {
+                    stack.Push(child);
+                }
+
+                result.Push(node.value);
+            }
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            var node = FindNodeWhitBfs(nodeKey);
+
+            if (node is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var parentNode = node.parent;
+
+            if (parentNode is null)
+            {
+                throw new ArgumentException();
+            }
+
+            parentNode.children.Remove(node);
+            node.parent = null;
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            var firstChild = FindNodeWhitBfs(firstKey);
+            var secondCild = FindNodeWhitBfs(secondKey);
+
+            if (firstChild == null || secondCild == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var firstChildParent = firstChild.parent;
+            var secondChildParent = secondCild.parent;
+
+            if (firstChildParent == null || secondChildParent == null)
+            {
+                throw new ArgumentException();
+            }
+
+            var indexOfFirstChildren = firstChildParent.children.IndexOf(firstChild);
+            var indexOfSecondChildren = secondChildParent.children.IndexOf(secondCild);
+
+            firstChildParent.children[indexOfFirstChildren] = secondCild;
+            secondCild.parent = firstChildParent;
+           
+            secondChildParent.children[indexOfSecondChildren] = firstChild;
+            firstChild.parent = secondChildParent;
+           
+        }
+    }
+}
